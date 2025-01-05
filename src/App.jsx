e1cf924f2a1105a9f35b15770edb99b203b739a9
iush12 <bhattaraiayush2003@gmail.com>
2025-01-05 23:39:03 +0545

    just deleting

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
 import Header from './components/Header'
 import Content from './components/Content'
-import Navbar from './components/Navbar'
-import Card from './components/Card'
 
-
-// const App = () => {
-//   const num =10
-//   return (
-//     <>
-//     <Card a={num} />
-//     <Card a="ayus"/>
-
-
-//     </>
-//   )
-// }
-
-// const App=()=>{
-//   const num=12;
-//   return(
-//     <>
-//     <h1>sda</h1>
-//     <Card a ={num}></Card>
-//     <Card b ="ayush"></Card>
-
-//     </>
-//   )
-// }
-// export default App
 
 import React from 'react'
 
